8b7195e739462d16ed5433d58afcd325bda26f60
kiz4ru <ladrongue5@gmail.com>
2024-10-14 21:29:01 +0200

    Subida de archivos definitivos

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# Gestion_Flick3r
+
+## Gestionar_Flickr_Session 
+![T](img/Flickr_logo.png)
+
+Esta aplicación permite iniciar sesión en Flickr utilizando OAuth y subir una foto a tu cuenta de Flickr.
+
+## Requisitos previos
+
+- Sistema operativo compatible (Windows, macOS, Linux)
+- Versión de [Node.js v21.1.0](https://nodejs.org) instalada
+- Otros requisitos específicos del proyecto (API key y API secret de flickr)
+
+## Instalación
+
+1. Clona el repositorio en tu máquina local:
+
+    ```bash
+    git clone https://github.com/kiz4ru/Gestionar_Flickr_Session.git
+    ```
+
+2. Navega al directorio del proyecto:
+
+    ```bash
+    cd tu-repositorio
+    ```
+
+
+3. Instala las dependencias del proyecto:
+
+    ```bash
+    npm install
+    ```
+
+## Configuración
+
+1. Crea un archivo de configuración `.env` en la raíz del proyecto.
+2. Completa las variables de entorno necesarias en el archivo `.env` según las instrucciones proporcionadas.
+3. Cambiar el id `photosetIdMontaje`, `photosetIdFinal`.
+Esto lo que hace es agregarte las fotos que vas subiendo al flickr a un album. Lo unico hay que tener un album creado y arriba de la url estara la id del album.
+Lineas 163-164
+## Uso
+
+1. Inicia la aplicación:
+
+    ```bash
+    node app.js
+    ```
+
+2. Se abrira automaticamente el `http://localhost:3000/authorize` para acceder a la aplicación.
---
+++ app.js
@@ -0,0 +1,408 @@
+const express = require('express');
+const querystring = require('querystring');
+const crypto = require('crypto');
+const cors = require('cors');
+const OAuth = require('oauth-1.0a');
+const fetch = require('node-fetch');
+const fs = require('fs');
+const FormData = require('form-data');
+const path = require('path');
+const session = require('express-session');
+require('dotenv').config();
+const app = express();
+const port = 3000;
+const apiKey = process.env.API_KEY;
+const apiSecret = process.env.API_SECRET;
+const redirectUri = 'http://localhost:3000/callback';
+let open;
+
+import('open').then((open) => {
+  open.default('http://localhost:3000/authorize');
+});
+
+
+const oauth = OAuth({
+  consumer: { key: apiKey, secret: apiSecret },
+  signature_method: 'HMAC-SHA1',
+  hash_function(base_string, key) {
+    return crypto.createHmac('sha1', key).update(base_string).digest('base64');
+  },
+});
+
+
+
+function getFilePaths(folderPath) {
+  let filePaths = [];
+  const filesInFolder = fs.readdirSync(folderPath);
+
+  for (const file of filesInFolder) {
+    const fullPath = path.join(folderPath, file);
+
+    if (fs.statSync(fullPath).isDirectory()) {
+      filePaths = [...filePaths, ...getFilePaths(fullPath)];
+    } else {
+      filePaths.push(fullPath);
+    }
+  }
+
+  return filePaths;
+}
+
+app.use(session({
+  secret: process.env.API_SECRET, // Llama a la variable de entorno SECRET desde el archivo .env
+  resave: false,
+  saveUninitialized: true,
+}));
+
+
+app.listen(port, () => {
+  console.log(`Servidor web iniciado en http://localhost:${port}`);
+});
+
+app.use(express.static('public'));
+app.use(cors());
+app.options('*', cors());
+
+
+app.get('/', (req, res) => {
+  const { oauth_token, oauth_verifier } = req.query;
+  if (oauth_token && oauth_verifier) {
+    res.redirect(`/callback?oauth_token=${oauth_token}&oauth_verifier=${oauth_verifier}`);
+    return;
+  }
+  res.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+
+app.get('/authorize', cors(), async (req, res) => {
+  try {
+    const request_data = {
+      url: 'https://www.flickr.com/services/oauth/request_token',
+      method: 'GET',
+      data: {
+        oauth_callback: redirectUri,
+        oauth_consumer_key: apiKey,
+        oauth_nonce: Math.floor(Math.random() * 1e12).toString(),
+        oauth_signature_method: 'HMAC-SHA1',
+        oauth_timestamp: Math.floor(Date.now() / 1000).toString(),
+        oauth_version: '1.0',
+      },
+    };
+
+    const authorization = oauth.authorize(request_data);
+    request_data.headers = { Authorization: constructAuthHeader(authorization) };
+
+    const response = await fetch(request_data.url, request_data);
+    if (!response.ok) {
+      throw new Error('Failed to authorize');
+    }
+
+    const responseData = await response.text();
+    const parsedData = querystring.parse(responseData);
+    const oauthToken = parsedData.oauth_token;
+    const oauthTokenSecret = parsedData.oauth_token_secret;
+
+    // Guarda oauthTokenSecret en la sesión
+    req.session.oauthTokenSecret = oauthTokenSecret;
+
+    res.redirect(`https://www.flickr.com/services/oauth/authorize?oauth_token=${oauthToken}&perms=write`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Failed to authorize');
+  }
+});
+
+
+
+app.get('/callback', cors(), async (req, res) => {
+  try {
+    const oauthToken = req.query.oauth_token;
+    const oauthVerifier = req.query.oauth_verifier;
+    
+    //recuperar de la sesion
+    const oauthTokenSecret = req.session.oauthTokenSecret;
+
+    const request_data = {
+      url: 'https://www.flickr.com/services/oauth/access_token',
+      method: 'POST',
+      data: {
+        oauth_consumer_key: apiKey,
+        oauth_nonce: Math.floor(Math.random * 1e12).toString(),
+        oauth_signature_method: 'HMAC-SHA1',
+        oauth_timestamp: Math.floor(Date.now() / 1000).toString(),
+        oauth_token: oauthToken,
+        oauth_verifier: oauthVerifier,
+        oauth_version: '1.0',
+        oauth_token_secret: oauthTokenSecret,
+      },
+    };
+    
+    
+      const authorization = oauth.authorize(request_data, { key: oauthToken, secret: oauthTokenSecret });
+      request_data.headers = { Authorization: constructAuthHeader(authorization) };
+
+const response = await fetch(request_data.url, request_data);
+if (!response.ok) {
+  throw new Error('Failed to get access token');
+}
+
+const responseData = await response.text();
+const parsedData = querystring.parse(responseData);
+const accessToken = parsedData.oauth_token;
+const accessTokenSecret = parsedData.oauth_token_secret;
+
+// Aquí puedes guardar accessToken y accessTokenSecret para usarlos en futuras solicitudes
+req.session.accessToken = accessToken;
+req.session.accessTokenSecret = accessTokenSecret;
+
+// Llamar a la API de Flickr
+await llamarApiFlickr(accessToken, accessTokenSecret);
+const imagePathsFinal = getFilePaths('C:\\Users\\desarrollador3\\Documents\\Proyectos\\FINAL');
+const imagePathsMontaje = getFilePaths('C:\\Users\\desarrollador3\\Documents\\Proyectos\\MONTAJE');
+
+const photosetIdFinal = '72177720313013105'; // Reemplaza con el ID de tu álbum "Final"
+const photosetIdMontaje = '72177720313025698'; // Reemplaza con el ID de tu álbum "Montaje"
+
+for (const imagePath of imagePathsFinal) {
+  await subirFotoAFlickr(accessToken, accessTokenSecret, imagePath, photosetIdFinal);
+}
+for (const imagePath of imagePathsMontaje) {
+  await subirFotoAFlickr(accessToken, accessTokenSecret, imagePath, photosetIdMontaje);
+}
+res.send(`
+      <html>
+        <head>
+          <style>
+            body {
+              background-color: #f0f0f0;
+              font-family: Arial, sans-serif;
+            }
+            .container {
+              max-width: 800px;
+              margin: 0 auto;
+              padding: 20px;
+            }
+            h1 {
+              color: #333;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="container">
+            <h1>successful</h1>
+          </div>
+        </body>
+      </html>
+    `);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Failed to get access token');
+  }
+  // Espera 5 segundos antes de intentar subir la foto
+  
+});
+
+
+// Define the request details
+const request_data = {
+  url: 'https://www.flickr.com/services/oauth/request_token',
+  method: 'POST',
+  data: {
+      oauth_callback: redirectUri
+  }
+};
+
+// Create the authorization header
+const authorizationHeader = oauth.toHeader(oauth.authorize(request_data));
+
+// Make the request
+fetch(request_data.url, {
+  method: request_data.method,
+  headers: authorizationHeader
+})
+.then(response => response.text())
+.then(data => {
+  // Handle the response
+  console.log(data);
+})
+.catch(error => {
+  // Handle the error
+  console.error('Error:', error);
+});
+
+async function llamarApiFlickr(accessToken, accessTokenSecret) {
+  console.log('accessToken:', accessToken);
+  console.log('accessTokenSecret:', accessTokenSecret);
+  try {
+    if (!accessToken || typeof accessToken !== 'string') {
+      throw new Error('Invalid or missing accessToken');
+    }
+    if (!accessTokenSecret || typeof accessTokenSecret !== 'string') {
+      throw new Error('Invalid or missing accessTokenSecret');
+    }
+
+    const oauthParams = {
+      oauth_consumer_key: apiKey,
+      oauth_nonce: Math.floor(Math.random() * 1e12).toString(),
+      oauth_signature_method: 'HMAC-SHA1',
+      oauth_timestamp: Math.floor(Date.now() / 1000).toString(),
+      oauth_token: accessToken,
+      oauth_version: '1.0',
+      format: 'json',
+      nojsoncallback: 1,
+      method: 'flickr.test.login'
+    };
+
+    const request_data = {
+      url: 'https://www.flickr.com/services/rest',
+      method: 'GET',
+      data: oauthParams
+    };
+
+    // Generar la firma OAuth
+    const authorization = oauth.authorize(request_data, { key: accessToken, secret: accessTokenSecret });
+
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        Authorization: constructAuthHeader(authorization)
+      },
+    };
+
+    const response = await fetch(`${request_data.url}?${new URLSearchParams(request_data.data).toString()}`, requestOptions);
+    if (!response.ok) {
+      console.log(response);
+      throw new Error('Failed to call Flickr API');
+    }
+
+    const responseData = await response.json();
+    console.log('Flickr API response:', responseData);
+  } catch (error) {
+    console.error(error);
+  }
+}
+function constructAuthHeader(params) {
+  return `OAuth ${Object.entries(params)
+    .map(([name, value]) => `${name}="${encodeURIComponent(value)}"`)
+    .join(', ')}`;
+}
+
+async function subirFotoAFlickr(accessToken, accessTokenSecret, imagePath, photosetId) {
+  try {
+    if (!accessToken || typeof accessToken !== 'string') {
+      throw new Error('Invalid or missing accessToken');
+    }
+    if (!accessTokenSecret || typeof accessTokenSecret !== 'string') {
+      throw new Error('Invalid or missing accessTokenSecret');
+    }
+    if (!imagePath || typeof imagePath !== 'string') {
+      throw new Error('Invalid or missing imagePath');
+    }
+
+    const oauthParams = {
+      oauth_consumer_key: apiKey,
+      oauth_nonce: Math.floor(Math.random() * 1e12).toString(),
+      oauth_signature_method: 'HMAC-SHA1',
+      oauth_timestamp: Math.floor(Date.now() / 1000).toString(),
+      oauth_token: accessToken,
+      oauth_version: '1.0',
+      format: 'json',
+      nojsoncallback: 1,
+      method: 'flickr.photo.upload'
+    };
+
+    const request_data = {
+      url: 'https://up.flickr.com/services/upload/',
+      method: 'POST',
+      data: oauthParams
+    };
+    
+    const form = new FormData();
+    form.append('photo', fs.createReadStream(imagePath));
+     //Añadir los parámetros OAuth al formulario
+      for (let key in oauthParams) {
+        form.append(key, oauthParams[key]);
+      }
+
+    // Generar la firma OAuth
+    const authorization = oauth.authorize(request_data, { key: accessToken, secret: accessTokenSecret });
+
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        ...form.getHeaders(),
+        //'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: constructAuthHeader(authorization)
+      },
+      body: form,
+    };
+    const xml2js = require('xml2js');
+    const response = await fetch(request_data.url, requestOptions);
+    if (!response.ok) {
+      console.log(response);
+      throw new Error('Failed to call Flickr API');
+    }
+
+    const responseData = await response.text();
+    console.log('Flickr API response:', responseData);
+
+    // Convertir la respuesta en un objeto
+    let responseObj;
+    xml2js.parseString(responseData, (err, result) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      responseObj = result;
+    });
+
+    // Añadir la foto al álbum
+    const photoId = responseObj.rsp.photoid[0]; // Extrae el photoId de la respuesta
+
+    await anadirfotoalbum(accessToken, accessTokenSecret, photoId, photosetId);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function anadirfotoalbum(accessToken, accessTokenSecret, photoId, photosetId) {
+  const oauthParams = {
+    oauth_consumer_key: apiKey,
+    oauth_nonce: Math.floor(Math.random() * 1e12).toString(),
+    oauth_signature_method: 'HMAC-SHA1',
+    oauth_timestamp: Math.floor(Date.now() / 1000).toString(),
+    oauth_token: accessToken,
+    oauth_version: '1.0',
+    format: 'json',
+    nojsoncallback: 1,
+    method: 'flickr.photosets.addPhoto',
+    photo_id: photoId,
+    photoset_id: photosetId
+  };
+
+  const request_data = {
+    url: 'https://www.flickr.com/services/rest',
+    method: 'POST',
+    data: oauthParams
+  };
+
+  const authorization = oauth.authorize(request_data, { key: accessToken, secret: accessTokenSecret });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: constructAuthHeader(authorization)
+    },
+    body:querystring.stringify(oauthParams),
+  };
+
+  const response = await fetch(request_data.url, requestOptions);
+  if (!response.ok) {
+    console.log(response);
+    throw new Error('Failed to call Flickr API');
+  }
+
+  const responseData = await response.json();
+  console.log('Flickr API response:', responseData);
+}
